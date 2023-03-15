7944d0596a65f7b682781a0d10529cb3b064695d
Angel Sainero Villar <angelsainero@gmail.com>
2023-03-15 19:24:10 +0100

    form validation and refactor

--- signup/signupController.js
+++ signup/signupController.js
@@ -1,24 +1,40 @@
 export function signupController(signupElement) {
   signupElement.addEventListener("submit", (event) => {
     //no queremos que haga el comportamiento por defecto (querystring en barra dir)
-    event.preventDefault();  
+    event.preventDefault();
 
-    
-    
-    //comprobar que las contraseñas sean iguales
-    const passwordElement = document.querySelector("#password");
-    const passwordElementConfirm = document.querySelector("#passwordConfirm");
-    
+    if (isFormValid(signupElement)) {
+      //crear usuario
+    }
+  });
+
+  //comprobar que las contraseñas sean iguales
+  function validPassword(signupElement) {
+    const passwordElement = signupElement.querySelector("#password");
+    const passwordElementConfirm =
+      signupElement.querySelector("#passwordConfirm");
+
     if (passwordElement.value !== passwordElementConfirm.value) {
-        alert("Las contraseñas no son iguales");
-    
+      alert("Las contraseñas no son iguales");
+      return false;
     }
-    //validamos email
-    const mailregExp = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
-    const emailElement = signupElement.querySelector('#username')   
+    return true;
+  }
+  //validamos email
+  function validEmail(signupElement) {
+    const mailregExp = new RegExp(
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    );
+    const emailElement = signupElement.querySelector("#username");
 
-    if(!mailregExp.test(emailElement.value)){
-        alert('El email no está bien escrito');
+    if (!mailregExp.test(emailElement.value)) {
+      alert("El email no está bien escrito");
+      return false;
     }
-});
+    return true;
+  }
+  //validamos formulario con los dos requisitos
+  function isFormValid(signupElement) {
+    return validEmail(signupElement && validPassword(signupElement));
+  }
 }
