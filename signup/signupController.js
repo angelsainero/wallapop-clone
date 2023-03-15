b019c66b21f7956aea189664ca3b1f8d4b74685d
Angel Sainero Villar <angelsainero@gmail.com>
2023-03-15 20:27:17 +0100

    create create user function

--- signup/signupController.js
+++ signup/signupController.js
@@ -3,38 +3,62 @@ export function signupController(signupElement) {
     //no queremos que haga el comportamiento por defecto (querystring en barra dir)
     event.preventDefault();
 
-    if (isFormValid(signupElement)) {
-      //crear usuario
+    const passwordElement = signupElement.querySelector("#password");
+    const passwordElementConfirm = signupElement.querySelector("#passwordConfirm");
+    const emailElement = signupElement.querySelector("#username");
+
+    if (
+      validEmail(emailElement.value) &&
+      validPassword(passwordElement.value, passwordElementConfirm.value)
+    ) {
+      createUser(emailElement.value, passwordElement.value);
     }
   });
 
   //comprobar que las contraseñas sean iguales
-  function validPassword(signupElement) {
-    const passwordElement = signupElement.querySelector("#password");
+  function validPassword(password, passwordConfirmation) {
     const passwordElementConfirm =
       signupElement.querySelector("#passwordConfirm");
 
-    if (passwordElement.value !== passwordElementConfirm.value) {
+    if (password !== passwordConfirmation) {
       alert("Las contraseñas no son iguales");
       return false;
     }
     return true;
   }
   //validamos email
-  function validEmail(signupElement) {
+  function validEmail(email) {
     const mailregExp = new RegExp(
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     );
-    const emailElement = signupElement.querySelector("#username");
 
-    if (!mailregExp.test(emailElement.value)) {
+    if (!mailregExp.test(email)) {
       alert("El email no está bien escrito");
       return false;
     }
     return true;
   }
-  //validamos formulario con los dos requisitos
-  function isFormValid(signupElement) {
-    return validEmail(signupElement && validPassword(signupElement));
+
+  async function createUser(email, password) {
+    //construir objeto con email y password siguiendo lo definido en el endpoint auth/register
+    const user = {
+      username: email,
+      password: password,
+    };
+    //consumir el api de sparrest usando un POST y enviando los datos que introducido el user
+    
+    await fetch('http://127.0.0.1:8000/auth/register', {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            "Content-Type":"application/json"
+        }
+    })
+    //gestionar la respuesta
+
+
+
+
+
   }
 }
